ca9bb04726c7237379448d0a3544f2c4c0df7168
Javiera Tapia <javierafrancisca92@gmail.com>
2019-03-05 05:27:28 -0300

    Added redux package, initial store, redux dev tools.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+// We import these two different stufF
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+// We invoke creation of the store
+// REDUX DEV TOOLS: https://github.com/zalmoxisus/redux-devtools-extension
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+// We inspect the store methods
+console.log(store.getState())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -42,28 +42,28 @@ const initialCalendarState = {
   },
 }
 
-// function calendar(state = initialCalendarState, action) {
-//   const { day, recipe, day } = action
-//   switch (action.type) {
-//     case ADD_RECIPE:
-//       return {
-//         ...state,
-//         [day]: {
-//           ...state[day],
-//           [meal]: recipe.label
-//         }
-//       }
-//     case REMOVE_FROM_CALENDAR:
-//       return {
-//         ...state,
-//         [day]: {
-//           ...state[day],
-//           [meal]: null
-//         }
-//       }
-//     default:
-//       return state
-//   }
-// }
+function calendar(state = initialCalendarState, action) {
+  const { day, recipe, meal } = action
+  switch (action.type) {
+    case ADD_RECIPE:
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label
+        }
+      }
+    case REMOVE_FROM_CALENDAR:
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null
+        }
+      }
+    default:
+      return state
+  }
+}
 
-// export default calendar
+export default calendar
